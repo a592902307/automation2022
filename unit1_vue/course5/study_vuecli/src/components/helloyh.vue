7cb0b413ce59c1f5f4062784d4fbad5e2c5d1b47
yh1995 <18358482401@163.com>
2022-09-07 12:42:50 +0800

    first commit

---
+++ unit1_vue/course5/study_vuecli/src/components/helloyh.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <button @click="chilema">吃饭/消化</button>
+        <h3 v-if="ate">吃饱了</h3>
+    </div>
+
+</template>
+
+<script>
+    // 选项式API
+    // export default {
+    //     name:'App',
+    //     data(){
+    //         let ate=false;
+    //         return{
+    //             ate
+    //         }
+    //     },
+    //     methods:{
+    //         chilema(){
+    //             this.ate=!this.ate
+    //         }
+    //     }
+    // }
+    // 组合式API
+    import {ref} from 'vue'
+    export default {
+        name: 'HelloYh',
+        // eslint-disable-next-line no-unused-vars
+        setup(props) {
+            let ate= ref(false) // 响应式数据需要通过ref包裹
+            console.log(ate.value) // false
+            function chilema(){
+                ate.value=!ate.value   // 数据类型需要通过value，组合式api中没有this
+            }
+            // 模板中引用的变量和方法统统都要return出去
+            return{
+                ate,
+                chilema
+            }
+        }
+    }
+
+</script>
---
+++ unit1_vue/course5/study_vuecli/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloYh></HelloYh>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import HelloYh from '../components/helloyh.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,HelloYh
+  }
+}
+</script>
